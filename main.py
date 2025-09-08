7f5218c9c6063546a3003523d45fdaf858411ca3
Zhou Young <zy856n@outlook.com>
2025-09-08 16:15:12 +0800

    Minor change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    return "Hello World!"
+    return "Hello World! This is my home page!"
 
 if __name__ == "__main__":
     app.run(debug=True)
